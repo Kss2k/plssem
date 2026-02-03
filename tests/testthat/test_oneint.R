fa2be9a946bc6e746b429d196f1d278b85b92bde
kss2k <slupphaugkjell@gmail.com>
2026-02-03 18:51:11 +0100

    Remove cSEM code

--- tests/testthat/test_oneint.R
+++ tests/testthat/test_oneint.R
@@ -1,5 +1,4 @@
 devtools::load_all()
-library(cSEM)
 
 m <- '
   X =~ x1 + x2 + x3
@@ -16,16 +15,3 @@ summary(fit)
 
 fit <- pls(m, oneIntOrdered, bootstrap = FALSE)
 summary(fit)
-
-mcsem <- '
-  X =~ x1 + x2 + x3
-  Z =~ z1 + z2 + z3
-  Y =~ y1 + y2 + y3
-
-  Y ~ X + Z + X.Z + X.X
-'
-
-fit.csem <- csem(.model = mcsem, .data = modsem::oneInt, .disattenuate = TRUE,
-               .resample_method = "none")
-summarize(fit.csem)
-calculateR2(fit.csem)

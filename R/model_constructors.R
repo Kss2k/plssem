aeb902140cdf25f48be06ca4f4cc074f48bd252d
kss2k <slupphaugkjell@gmail.com>
2026-04-28 21:51:38 +0200

    Remove constructor for `PlsBaseModel` (no longer in use)

--- R/model_constructors.R
+++ R/model_constructors.R
@@ -1,44 +1,18 @@
-PlsBaseModel <- function(matrices       = matrix(),
-                         data           = matrix(),
-                         info           = list(),
-                         status         = list(),
-                         params         = list(),
-                         fit            = list(),
-                         fitConsistent  = list(),
-                         fitUncorrected = list(),
-                         fitLmer        = list(),
-                         factorScores   = NULL) {
-
-  methods::new("PlsBaseModel",
-    matrices       = matrices,
-    data           = data,
-    info           = info,
-    status         = status,
-    params         = params,
-    fit            = fit,
-    fitConsistent  = fitConsistent,
-    fitUncorrected = fitUncorrected,
-    fitLmer        = fitLmer,
-    factorScores   = factorScores
-  )
-}
-
-
-PlsModel <- function(matrices        = list(),
-                     data            = matrix(),
-                     info            = list(),
-                     status          = list(),
-                     params          = list(),
-                     fit             = list(),
-                     fitConsistent   = list(),
-                     fitUncorrected  = list(),
-                     fitLmer         = list(),
-                     factorScores    = NULL,
-                     parTableInput   = data.frame(),
+PlsModel <- function(matrices         = list(),
+                     data             = matrix(),
+                     info             = list(),
+                     status           = list(),
+                     params           = list(),
+                     fit              = list(),
+                     fitConsistent    = list(),
+                     fitUncorrected   = list(),
+                     fitLmer          = list(),
+                     factorScores     = NULL,
+                     parTableInput    = data.frame(),
                      higherOrderModel = NULL,
-                     combinedModel   = NULL,
-                     parTable        = NULL,
-                     boot            = list()) {
+                     combinedModel    = NULL,
+                     parTable         = NULL,
+                     boot             = list()) {
 
   methods::new("PlsModel",
     matrices         = matrices,

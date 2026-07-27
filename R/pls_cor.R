5b099b44aa3df452ca85bce4ed33893c867b1159
kss2k <slupphaugkjell@gmail.com>
2026-07-27 09:57:10 +0200

    Safely handle errors and missing values in `plsCor()`

--- R/pls_cor.R
+++ R/pls_cor.R
@@ -1,4 +1,7 @@
 plsCor <- function(data, ordered = NULL) {
+  if (!is.data.frame(data))
+    data <- data.frame(data)
+
   vars <- colnames(data)
   ord  <- vars %in% ordered
   k    <- length(vars)
@@ -18,6 +21,29 @@ plsCor <- function(data, ordered = NULL) {
     data[,i] <- y
   }
 
+  trycor <- function(.f, type) {
+    function(...) {
+      tryCatch(.f(...), error = function(e) {
+        pls_msg_warn(
+          "Estimation of", type, "correlation failed!",
+          "Message:", conditionMessage(e)
+        )
+        NaN
+      })
+    }
+  }
+
+  hasNA <- any(!stats::complete.cases(data))
+
+  pls_warnif(hasNA,
+    "Found missing values in data!",
+    "Using pairwise complete observations..."
+  )
+
+  polychor   <- trycor(plsPolychor, "polychoric")
+  pearson    <- trycor(cor, "Pearson")
+  polyserial <- trycor(plsPolyserial, "polyserial")
+
   for (i in seq_len(k)) {
     ord.i <- ord[[i]]
     x <- data[,i, drop = TRUE]
@@ -26,14 +52,23 @@ plsCor <- function(data, ordered = NULL) {
       ord.j <- ord[[j]]
       y <- data[,j, drop = TRUE]
 
-      if (!ord.i && !ord.j) {
-        rho <- cor(x, y)
-      } else if (ord.i && ord.j) {
-        rho <- plsPolychor(x, y)
-      } else if (ord.i && !ord.j) {
-        rho <- plsPolyserial(y, x) 
-      } else { # !ord.i && ord.j
-        rho <- plsPolyserial(x, y) 
+      if (hasNA) {
+        .x <- x
+        ok <- !is.na(x) & !is.na(y)
+        x  <- x[ok]
+        y  <- y[ok]
+      }
+
+      if      (!ord.i && !ord.j) rho <- pearson(x, y)
+      else if ( ord.i &&  ord.j) rho <- polychor(x, y)
+      else if ( ord.i && !ord.j) rho <- polyserial(y, x) 
+      else if (!ord.i &&  ord.j) rho <- polyserial(x, y) 
+      else pls_msg_stop( # Should be impossible
+        "Unexpected condition in `plsCor()`, this is likely a bug!"
+      )
+
+      if (hasNA) {
+        x <- .x # restore in case we removed observations
       }
 
       S[i, j] <- S[j, i] <- rho
